cee8f379c6d68301665d3e8a1184303e94bbe1ba
abhi@96k <abhisheknangare96k@gmail.com>
2024-08-05 22:44:13 +0530

    app changes

--- src/app.js
+++ src/app.js
@@ -1,39 +1,51 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true, // Allow credentials (cookies)
-}))
-app.use(cookieParser())
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (
+        [
+          "https://frontend-study-flicks.vercel.app",
+          "http://localhost:5173",
+        ].includes(origin)
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+
+app.use(cookieParser());
 // built-in middleware for parsing JSON bodies
-app.use(express.json({limit: "10kb"}))
+app.use(express.json({ limit: "10kb" }));
 
-app.use(express.urlencoded({extended: true, limit: '10kb'}))
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
-app.use(express.static("public"))
-
-
-
+app.use(express.static("public"));
 
 //routes import
-import userRouter from './routes/user.routes.js'
-import videoRouter from './routes/video.routes.js'
-import playlistRouter from './routes/playlist.routes.js'
-import commentRouter from './routes/comment.routes.js'
-import subscriptionRouter from './routes/subscription.routes.js'
-import likeRouter from './routes/like.routes.js'
-import notificationRouter from './routes/notification.routes.js'
+import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import notificationRouter from "./routes/notification.routes.js";
 
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/videos", videoRouter)
-app.use("/api/v1/playlist", playlistRouter)
-app.use("/api/v1/comments", commentRouter)
-app.use("/api/v1/subscription", subscriptionRouter)
-app.use('/api/v1/likes', likeRouter);
-app.use('/api/v1/notifications', notificationRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/notifications", notificationRouter);
 
-export {app}
+export { app };
